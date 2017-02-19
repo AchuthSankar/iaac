38cb7791e7389449bd66b455af0f8fa194330741
achuth <achuth@localhost>
2017-02-20 03:14:13 +0530

    s

--- docker_image_deploy.sh
+++ docker_image_deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 docker --tlsverify -H tcp://192.168.99.100:2376 build -t achuthman/$ServiceName .
-status=\$?
-if [ \$status -ne "0" ]; then
+status=$?
+if [ $status -ne "0" ]; then
 	echo "Docker build failed"
 	exit -1
 fi
 docker --tlsverify -H tcp://192.168.99.100:2376 rm -f $ServiceName
 docker --tlsverify -H tcp://192.168.99.100:2376 run -d --name $ServiceName achuthman/$ServiceName
-status=\$?
-if [ \$status -ne 0 ]; then
+status=$?
+if [ $status -ne 0 ]; then
 	echo "Docker run failed"
 	exit -1
 fi
